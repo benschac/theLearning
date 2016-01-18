bcdcea6ffc7a659cece7c0a967d15bf9919dcedf
Bejamin Schachter <benschac@gmail.com>
2016-01-18 11:52:35 -0500

    update controller and data in model

--- angular_by_example/2chapt/js/7minWorkout/workout.js
+++ angular_by_example/2chapt/js/7minWorkout/workout.js
@@ -37,7 +37,184 @@ function WorkoutPlan(args) {
 var restExercise;
 var workoutPlan;
 
+var createWorkout = function() {
+  // instanciate new Workout Object.
+  var workout = new WorkoutPlan({
+    name: "7minWorkout",
+    title: "7 Minute Workout",
+    restBetweenExercise: 10
+  });
 
+  // push an exercise object into workout's exercise array.
+  // this could come from a third party API or persistent data resource.
+  // this is just adding "content".
+  workout.exercise.push({
+    details: new Exercise({
+      name: "jumping jacks",
+      title: "Jumping Jacks",
+      description: "Jumping Jacks.",
+      image: "img/JumpingJacks.png",
+      videos: [],
+      variations: [],
+      procedure: "";
+    }),
+
+    duration: 30
+  });
+
+  workout.exercises.push({
+    exercise: new Exercise({
+        name: "wallSit",
+        title: "Wall Sit",
+        description: "Wall Sit.",
+        image: "img/wallsit.png",
+        videos: [],
+        variations: [],
+
+    }),
+    duration: 30
+  });
+  workout.exercises.push({
+      exercise: new Exercise({
+          name: "pushUp",
+          title: "Push Up",
+          description: "Discription about pushup.",
+          image: "img/pushup.png",
+          videos: ["https://www.youtube.com/watch?v=Eh00_rniF8E", "https://www.youtube.com/watch?v=ZWdBqFLNljc", "https://www.youtube.com/watch?v=UwRLWMcOdwI", "https://www.youtube.com/watch?v=ynPwl6qyUNM", "https://www.youtube.com/watch?v=OicNTT2xzMI"],
+          variations: ["Planche push-ups", "Knuckle push-ups", "Maltese push-ups", "One arm versions"],
+          procedure: ""
+      }),
+      duration: 30
+  });
+  workout.exercises.push({
+      exercise: new Exercise({
+          name: "crunches",
+          title: "Abdominal Crunches",
+          description: "Abdominal Crunches.",
+          image: "img/crunches.png",
+          videos: [],
+          variations: [],
+          procedure: ""
+      }),
+      duration: 30
+  });
+  workout.exercises.push({
+      exercise: new Exercise({
+          name: "stepUpOntoChair",
+          title: "Step Up Onto Chair",
+          description: "Step Up Onto Chair.",
+          image: "img/stepUpOntoChair.jpeg",
+          videos: [],
+          variations: [],
+          procedure: ""
+      }),
+      duration: 30
+  });
+  workout.exercises.push({
+      exercise: new Exercise({
+          name: "squat",
+          title: "Squat",
+          description: "Squat.",
+          image: "img/squat.png",
+          videos: [],
+          variations: [],
+          procedure: ""
+      }),
+      duration: 30
+  });
+  workout.exercises.push({
+      exercise: new Exercise({
+          name: "tricepdips",
+          title: "Tricep Dips On Chair",
+          description: "Tricep Dips On Chair.",
+          image: "img/tricepdips.jpg",
+          videos: [],
+          variations: [],
+          procedure: ""
+      }),
+      duration: 30
+  });
+  workout.exercises.push({
+      exercise: new Exercise({
+          name: "plank",
+          title: "Plank",
+          description: "Plank.",
+          image: "img/plank.png",
+          videos: [],
+          variations: [],
+          procedure: ""
+      }),
+      duration: 30
+  });
+  workout.exercises.push({
+      exercise: new Exercise({
+          name: "highKnees",
+          title: "High Knees",
+          description: "High Knees.",
+          image: "img/highknees.png",
+          videos: [],
+          variations: [],
+          procedure: ""
+      }),
+      duration: 30
+  });
+  workout.exercises.push({
+      exercise: new Exercise({
+          name: "lunges",
+          title: "Lunges",
+          description: "Lunges.",
+          image: "img/lunges.png",
+          videos: [],
+          variations: [],
+          procedure: ""
+      }),
+      duration: 30
+  });
+  workout.exercises.push({
+      exercise: new Exercise({
+          name: "pushupNRotate",
+          title: "Pushup And Rotate",
+          description: "Pushup And Rotate.",
+          image: "img/pushupNRotate.jpg",
+          videos: [],
+          variations: [],
+          procedure: ""
+      }),
+      duration: 30
+  });
+  workout.exercises.push({
+      exercise: new Exercise({
+          name: "sidePlank",
+          title: "Side Plank",
+          description: "Side Plank.",
+          image: "img/sideplank.png",
+          videos: [],
+          variations: [],
+          procedure: ""
+      }),
+      duration: 30
+  });
+}
+
+
+var startWorkout = function() {
+  // define workoutPlan and create overall plan.
+  var workoutPlan = createWorkout();
+
+  // define restExercise
+  restExercise = {
+    // use model constructor function.
+    details: new Exercise({
+      name: "rest",
+      title: "Relax!",
+      description: "Take a break!",
+      image: "img/rest.png"
+    }),
+    duration: workoutPlan.restBetweenExercise;
+  };
+  // start && use / shift off the first exercise in the array.
+  startExercise(workoutPlan.exercises.shift());
+}
 
 // declare init function.
 var init = function() {
