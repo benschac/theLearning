caea904df4c982230694efa6cf8148a32176b5e0
Benjamin Schachter <benschac@gmail.com>
2016-01-24 15:48:47 -0500

    update routes

--- angular_by_example/2chapt/7minWorkout/app.js
+++ angular_by_example/2chapt/7minWorkout/app.js
@@ -1,5 +1,24 @@
 // root module
-angular.module('app', ['7minWorkout']);
+angular.module('app', ['ngRoute','7minWorkout'])
+.config(function($routeProvider) {
+  $routeProvider.when('/start', {
+    templateURL: 'partials/start.html'
+  });
+
+  $routeProvider.when('/workout', {
+    templateURL: 'partials/workout.html',
+    controller: 'WorkoutController'
+  });
+
+  $routeProvider.when('/finish', {
+    templateURL: 'partials/finish.html'
+  });
+
+  $routeProvider.otherwise({
+    redirectTo: '/start'
+  });
+
+})
 // feature based module
 angular.module('7minWorkout', []);
 
