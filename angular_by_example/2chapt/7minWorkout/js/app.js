99f0f4c6b5df655cfd95700978aced854baed773
Benjamin Schachter <benschac@gmail.com>
2016-02-07 11:28:19 -0500

    temp fix with location provider

--- angular_by_example/2chapt/7minWorkout/js/app.js
+++ angular_by_example/2chapt/7minWorkout/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('app', ['ngRoute','7minWorkout'])
 .config(['$routeProvider', function($routeProvider) {
-  $locationProvider.html5Mode(true);
+  // $locationProvider.html5Mode(true);
 
   $routeProvider.when('/start', { templateUrl: 'partials/start.html' });
   $routeProvider.when('/workout', { templateUrl: 'partials/workout.html', controller: 'WorkoutController' });
