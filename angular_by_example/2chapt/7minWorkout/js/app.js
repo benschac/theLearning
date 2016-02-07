6e3ec14e7a78329e61715ae3497db6fc77a9f080
Benjamin Schachter <benschac@gmail.com>
2016-02-07 16:55:05 -0500

    stop at page 97

--- angular_by_example/2chapt/7minWorkout/js/app.js
+++ angular_by_example/2chapt/7minWorkout/js/app.js
@@ -3,8 +3,13 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('app', ['ngRoute','7minWorkout'])
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', '$sceDelegateProvider', function($routeProvider, $sceDelegateProvider) {
   // $locationProvider.html5Mode(true);
+
+  $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    'http://*.youtube.com/**'
+  ]);
 
   $routeProvider.when('/start', { templateUrl: 'partials/start.html' });
   $routeProvider.when('/workout', { templateUrl: 'partials/workout.html', controller: 'WorkoutController' });
