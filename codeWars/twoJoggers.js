1cc602ae61d12c84b5d5e4c0e626d6616c0602d7
Bejamin Schachter <benschac@gmail.com>
2016-07-10 13:48:41 -0400

    codeWars of the day 7/10/2016

---
+++ codeWars/twoJoggers.js
@@ -0,0 +1,34 @@
+/*
+	7/10/2016
+	https://www.codewars.com/kata/two-joggers/javascript
+	5 kyu;
+	Notes: Todo -- refactor code.
+	1. condense for loops into function.
+	2. Use Math object methods to condense conditional.
+
+*/
+
+
+
+var nbrOfLaps = function (x, y) {
+var arr = [];
+	 function common() {
+	  
+	    for(var i = x; i < (x * y); i+= x) {
+	      if(i % x === 0 && i % y === 0) {
+        break;
+	      }
+	    }
+	    
+	    for(var j = y; j < (x * y); j+= y) {
+	      if(j % x === 0 && j % y === 0) {
+        break;
+	      }
+	    }
+	    arr.push((i / x));
+	    arr.push((j / y));
+	    return arr;
+	 }
+	  
+  return common();
+};
