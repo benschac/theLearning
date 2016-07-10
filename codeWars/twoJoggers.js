d2a94db3c03c0f4549a7a9a5f91d8ad21cc0a651
Bejamin Schachter <benschac@gmail.com>
2016-07-10 14:00:33 -0400

    formatting update

--- codeWars/twoJoggers.js
+++ codeWars/twoJoggers.js
@@ -14,22 +14,22 @@ var nbrOfLaps = function (x, y) {
 	var arr = [];
 		function common() {
 
-		for(var i = x; i < (x * y); i+= x) {
-			if(i % x === 0 && i % y === 0) {
-			break;
+			for(var i = x; i < (x * y); i+= x) {
+				if(i % x === 0 && i % y === 0) {
+				break;
+				}
 			}
-		}
 
-		for(var j = y; j < (x * y); j+= y) {
-			if(j % x === 0 && j % y === 0) {
-			break;
+			for(var j = y; j < (x * y); j+= y) {
+				if(j % x === 0 && j % y === 0) {
+				break;
+				}
 			}
-		}
 
-		arr.push((i / x));
-		arr.push((j / y));
+			arr.push((i / x));
+			arr.push((j / y));
 
-		return arr;
+			return arr;
 		
 		}
 
