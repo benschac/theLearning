ccd83676c60848519a793028274200ea72432d29
Bejamin Schachter <benschac@gmail.com>
2016-11-21 14:23:00 -0500

    finish form UI layer

--- front-end-dev-book/coffeeRun/scripts/main.js
+++ front-end-dev-book/coffeeRun/scripts/main.js
@@ -2,16 +2,24 @@
   'use strict';
 
   var FORM_SELECTOR = '[data-coffee-order="form"]';
+  var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
-
+  var CheckList = App.CheckList;
   var truck = new Truck('ncc-1701', new DataStore());
 
   window.truck = truck;
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
+  checkList.addClickHandler(truck.deliverOrder.bind(truck));
   var formHandler = new FormHandler(FORM_SELECTOR);
 
-  formHandler.addSubmitHandler(truck.createOrder.bind(truck));
-  console.log(formHandler);
+  formHandler.addSubmitHandler(function(data) {
+    // envoke both truck and checklist methods on
+    // submit.  Pass instance to change with data.
+    truck.createOrder.call(truck, data);
+    checkList.addRow.call(checkList, data);
+  });
+  
 })(window);
